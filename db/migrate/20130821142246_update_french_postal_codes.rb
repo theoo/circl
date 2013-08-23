e19b6af5623649af966f6538f8c1c575df532887
Théo Reichel <theo@complex.ch>
2013-08-23 11:10:32 +0200

    Update last migration for vanilla directories.

--- db/migrate/20130821142246_update_french_postal_codes.rb
+++ db/migrate/20130821142246_update_french_postal_codes.rb
@@ -1,21 +1,23 @@
 class UpdateFrenchPostalCodes < ActiveRecord::Migration
   def change
-  	locations_count = Location.where(:name => "France")
-			.first
-			.children
-			.where("locations.postal_code_prefix ~ '^.{4}$' ").count
+  	france = Location.where(:name => "France").first
 
-		puts "Updating #{locations_count} locations."
-    @bar = RakeProgressbar.new(locations_count)
-  	(1..9).each do |num|
-  		Location.where(:name => "France")
-  			.first
-  			.children
-  			.where("locations.postal_code_prefix ~ '^#{num}.{3}$' ").each do |departement|
-  				departement.update_attribute :postal_code_prefix, "0" + departement.postal_code_prefix
-  				@bar.inc
-  			end
-  	end
-  	@bar.finished
+    if france
+      locations_count = france.children
+			                    .where("locations.postal_code_prefix ~ '^.{4}$' ").count
+
+  		puts "Updating #{locations_count} locations."
+      @bar = RakeProgressbar.new(locations_count)
+    	(1..9).each do |num|
+    		Location.where(:name => "France")
+    			.first
+    			.children
+    			.where("locations.postal_code_prefix ~ '^#{num}.{3}$' ").each do |departement|
+    				departement.update_attribute :postal_code_prefix, "0" + departement.postal_code_prefix
+    				@bar.inc
+    			end
+    	end
+    	@bar.finished
+    end
   end
 end
