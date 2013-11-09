1efbc3cf42e55b401d8ccaf73bec617c8b3d59be
Théo Reichel <theo@complex.ch>
2013-11-09 21:54:29 +0100

    Add rake db:seed:*:upgrade to seed_helper

--- lib/seed_helper.rb
+++ lib/seed_helper.rb
@@ -19,6 +19,19 @@ module SeedHelper
             puts 'done!'
           end
 
+          desc "Add missing application settings keys"
+          task :upgrade => :environment do
+            print "Upgrading #{name}... "
+            YAML.load_file("#{Rails.root}/db/seeds/#{name}.yml").each do |h|
+              h.keys.each do |k|
+                if name.to_s.singularize.camelize.constantize.where(k.to_sym => h[k]).size == 0
+                  name.to_s.singularize.camelize.constantize.create!(h)
+                end
+              end
+            end
+            puts 'done!'
+          end
+
           scope = namespace.instance_variable_get('@scope').to_a.join(':')
           desc "resets #{name} (destroys then creates)"
           task :reset => ["#{scope}:destroy", "#{scope}:create"]
