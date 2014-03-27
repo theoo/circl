6762223b2c2f249c2d9a2bfb0b9e2053d314892b
Théo Reichel <theo@complex.ch>
2014-03-27 22:29:50 +0100

    Enable the ability to disable geoloc (backport)

--- app/models/person.rb
+++ app/models/person.rb
@@ -697,15 +697,20 @@ class Person < ActiveRecord::Base
   end
 
   def update_geographic_coordinates
-    # FIXME remove this condition after 20131125214900_add_geolocalization migration
-    if Person.columns.map(&:name).include?("latitude")
-      if (address_changed? or location_id_changed?) or (address and location_id and latitude.nil?)
-        loc = Geokit::Geocoders::OSMGeocoder.geocode full_address_inline
-        if loc.success
-          self.latitude = loc.lat
-          self.longitude = loc.lng
+    begin
+      # FIXME remove this condition after 20131125214900_add_geolocalization migration
+      if Person.columns.map(&:name).include?("latitude") \
+        and Rails.configuration.settings['maps']['enable_geolocalization']
+        if (address_changed? or location_id_changed?) or (address and location_id and latitude.nil?)
+          loc = Geokit::Geocoders::OSMGeocoder.geocode full_address_inline
+          if loc.success
+            self.latitude = loc.lat
+            self.longitude = loc.lng
+          end
         end
       end
+    rescue
+      # NOTE do not fail, on network failure for instance
     end
   end
 
