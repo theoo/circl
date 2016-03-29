83df2c3d5b9151fce304a8badba4a45535cb28b3
naveenputta <navinkumar.putta@gmail.com>
2016-03-29 18:14:17 +0200

    Changed the way of calling snapshot action.

--- spec/jobs/generate_generic_template_jpg_rspec.rb
+++ spec/jobs/generate_generic_template_jpg_rspec.rb
@@ -3,8 +3,11 @@ require 'spec_helper'
 describe GenerateGenericTemplateJpg do
   it "should generate generic template jpg for person" do
   	generic_template = FactoryGirl.create(:generic_template, :class => GenericTemplate)
-  	take_snapshot1 = generic_template.take_snapshot
-  	take_snapshot2 = GenerateGenericTemplateJpg.perform(generic_template.id)
+  	generic_template.take_snapshot
+  	take_snapshot1 = generic_template.snapshot
+  	GenerateGenericTemplateJpg.perform(generic_template.id)
+  	generic_template.reload
+  	take_snapshot2 = generic_template.snapshot
   	expect(take_snapshot1).to eq(take_snapshot2)
   end
 end
--- spec/jobs/generate_invoice_template_jpg_rspec.rb
+++ spec/jobs/generate_invoice_template_jpg_rspec.rb
@@ -3,8 +3,11 @@ require 'spec_helper'
 describe GenerateInvoiceTemplateJpg do
   it "should generate invoice template jpg for person" do
   	invoice_template = FactoryGirl.create(:invoice_template)
-  	take_snapshot1 = invoice_template.take_snapshot
-  	take_snapshot2 = GenerateInvoiceTemplateJpg.perform(invoice_template.id)
+  	invoice_template.take_snapshot
+  	take_snapshot1 = invoice_template.snapshot
+  	GenerateInvoiceTemplateJpg.perform(invoice_template.id)
+  	invoice_template.reload
+  	take_snapshot2 = invoice_template.snapshot
   	expect(take_snapshot1).to eq(take_snapshot2)
   end
 end
