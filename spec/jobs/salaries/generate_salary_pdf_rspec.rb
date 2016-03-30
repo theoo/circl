3a5b79ca4fa409a0029f0cf8f2601cd16c70673f
naveenputta <navinkumar.putta@gmail.com>
2016-03-30 18:18:21 +0200

    Added spec for generate salary pdf

--- spec/jobs/salaries/generate_salary_pdf_rspec.rb
+++ spec/jobs/salaries/generate_salary_pdf_rspec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 
-describe GenerateSalaryPdf do
+describe Salaries::GenerateSalaryPdf do
   it "should generate salaries pdf" do
-  	
-  	salary = FactoryGirl.create(:salary)
-
+  	salary = FactoryGirl.create(:salary, :class => Salaries::Salary)
+  	generator = AttachmentGenerator.new(salary)
+  	generator_pdf = generator.update_attributes pdf: generator.pdf
+  	Salaries::GenerateSalaryPdf.perform(salary.id)
+  	expect(ES.search(generator).pdf).be eq(generator_pdf)
   end
 end
